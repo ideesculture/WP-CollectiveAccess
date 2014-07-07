57dc20641dae3ee808d926a0ab0999759e89da16
GautierMichelin <gm@ideesculture.com>
2014-07-07 16:33:25 +0200

    Adding cache db table creation

--- WP-CollectiveAccess.php
+++ WP-CollectiveAccess.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP-CollectiveAccess
  * Plugin URI: http://github.com/ideesculture/wp-collectiveaccess
- * Description: WordPress connector to display CollectiveAccess data & medias in Wordpress
+ * Description: Display CollectiveAccess data & medias in Wordpress
  * Version: 0.1
  * Author: Gautier Michelin, idéesculture.
  * Funding : Project originally funded by Pro-Memoria
@@ -31,46 +31,67 @@ $vp =  new Virtual_Themed_Pages_BC();
 // Including admin page file
 require_once(plugin_dir_path( __FILE__ ) ."collectiveaccess_settings.php");
 
-// Including details functions
+// Including details & search functions
 require_once(plugin_dir_path( __FILE__ ) ."collectiveaccess_object_detail.php");
-
-// Including search functions
 require_once(plugin_dir_path( __FILE__ ) ."collectiveaccess_objects_search.php");
+require_once(plugin_dir_path( __FILE__ ) ."collectiveaccess_browse.php");
 
 // Requiring widget functions
 require_once(plugin_dir_path( __FILE__ ) ."widget_random_object.php");
 require_once(plugin_dir_path( __FILE__ ) ."widget_search_form.php");
 require_once(plugin_dir_path( __FILE__ ) ."widget_browse_links.php");
 
-// Install & uninstall functions
-register_activation_hook( __FILE__, "collectiveacces_install" );
+// Install & update functions
+register_activation_hook( __FILE__, "collectiveaccess_install" );
+//add_action( 'plugins_loaded', 'collectiveaccess_update_db_check' );
+
+global $wpca_version;
+$wpca_version = "0.1";
+
+
+/**
+ *
+ */
 function collectiveaccess_install() {
-    // if version < 3.9, deactivate our plugin (not tested with a version under 3.9)
+	global $wpca_version, $wpdb;
+	// Force DB install
+	update_option( "wpca_version", "" );
+	$installed_version = get_option( "wpca_version" );
+
+	// if version < 3.9, deactivate our plugin (not tested with a version under 3.9)
     if( version_compare( get_bloginfo( "version" ), "3.9", "<" ) ) {
         deactivate_plugins( basename( __FILE__ ) );
     }
+	if($installed_version != $wpca_version) {
+		$table_name = $wpdb->prefix . "collectiveaccess_cache";
+		$sql = "CREATE TABLE $table_name (
+  id mediumint(9) NOT NULL AUTO_INCREMENT,
+  service tinytext NOT NULL,
+  base_url text NOT NULL,
+  catable tinytext NOT NULL,
+  mode tinytext,
+  query text,
+  time datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  result blob,
+  UNIQUE KEY id (id)
+    );";
 
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		update_option( "wpca_version", $wpca_version );
+	}
     $collectiveaccess_options = array(
         "view" => "grid" ,
         "food" => "bacon",
         "mode" => "zombie"
     );
     update_option("collectiveaccess_options", $collectiveaccess_options);
-
-	/* CREATE wp_ca_cache :
-	CREATE TABLE `wp_ca_cache` (
-	  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-	  `service` text,
-	  `table` text,
-	  `target` text,
-	  `json` longblob,
-	  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	  PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-	*/
 }
 
 function collectiveaccess_uninstall() {
-
+	global $wpca_version;
+	if (get_site_option( 'wpca_version' ) != $wpca_version) {
+		collectiveaccess_install();
+	}
 }
