2e3d632980fac254c40b6a9f3d7ae9e20f073d81
GautierMichelin <gm@ideesculture.com>
2014-07-13 18:53:42 +0200

    Adding an ajax handler

---
+++ collectiveaccess_ajax_handler.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * WP-CollectiveAccess
+ * User: gautier
+ * Date: 13/07/2014
+ * Time: 10:21
+ *
+ * Simple AJAX handler, no need to use admin-ajax.php since almost none of our data is inside WordPress
+ * Origin :  http://www.coderrr.com/create-your-own-admin-ajax-php-type-handler/ by Brian Fegter
+ * (Little) Adaptation Gautier Michelin
+ *
+ */
+
+// Requiring ajax handlers
+//require_once(__DIR__."/collectiveaccess_ajax_getimage.php");
+
+require_once(__DIR__."/collectiveaccess_common_functions.php");
+
+define('DOING_AJAX', true);
+
+if (!isset( $_POST['action']))
+    die('-1');
+
+//relative to where your plugin is located
+require_once('../../../wp-load.php');
+
+//Typical headers
+header('Content-Type: text/html');
+send_nosniff_header();
+
+//Disable caching
+header('Cache-Control: no-cache');
+header('Pragma: no-cache');
+
+$action = esc_attr($_POST['action']);
+
+//A bit of security : the allowed_actions array must contain all allowed actions
+$allowed_actions = array(
+    'getimage'
+);
+
+
+
+$test = add_action('collectiveaccess_ajax_getimage', 'collectiveaccess_ajax_getimage');
+
+function collectiveaccess_ajax_getimage()
+{
+    // Checking if XHR (ajax) request
+    $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+    if(!$isAjax) die('-1');
+
+    $result = collectiveaccess_getpreviewimage($_REQUEST[table], $_REQUEST[id]);
+    echo json_encode($result);
+
+}
+
+if(in_array($action, $allowed_actions)) {
+    do_action('collectiveaccess_ajax_'.$action);
+} else {
+    die('-1');
+}
