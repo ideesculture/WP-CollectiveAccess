9067f2c4669cdd5b3da842d0aeddbd29a09aa118
GautierMichelin <gm@ideesculture.com>
2014-07-14 10:32:45 +0200

    Removing non-needed var, moving css & JS at top of the function for more readability

--- collectiveaccess_objects_search.php
+++ collectiveaccess_objects_search.php
@@ -17,6 +17,50 @@ function collectiveaccess_search_results($v, $url)
 {
     global $wpdb;
 
+    // for more comprehensive readability, CSS & JS insertion are included here, PHP function code is after
+    $cssAndScript =
+"<style>
+figure.gallery-item p {display:none;}
+
+.collectiveaccess-cropping-image {
+    height:170px;
+    width:150px;
+    overflow:hidden;
+    background-color: black;
+    text-align: center;
+    vertical-align: middle;
+    display:table-cell;
+}
+</style>
+<script type='text/javascript'>
+    jQuery(document).ready(function() {
+        jQuery('img.attachment-thumbnail').each(function() {
+            var getimage_table = jQuery(this).data(\"table\");
+            var getimage_id = jQuery(this).data(\"id\");
+            var jquery_this = jQuery(this);
+            jQuery.ajax({
+                type:'POST',
+                dataType: 'text',
+                url:'" . get_site_url() . "/wp-content/plugins/WP-CollectiveAccess/collectiveaccess_ajax_handler.php',
+                data: {
+                    action:'getimage',
+                    table:getimage_table,
+                    id:getimage_id
+                },
+                success:function(response){
+                    if(response != '-1') {
+                        //console.log(response);
+                        jquery_this.attr('src',JSON.parse(response));
+                    } else {
+                        jquery_this.removeAttr('src');
+                    }
+                }
+            });
+        });
+    });
+
+</script>";
+
     // getting query from post or get
     if (!($query=$_POST["query"])) $query=$_GET["query"];
     if (!($page=$_POST["page"])) $page=1;
@@ -27,12 +71,10 @@ function collectiveaccess_search_results($v, $url)
     $options = get_option('collectiveaccess_options');
     //var_dump($options);die();
 
-    $url_base = empty( $options[url_base] ) ? 'localhost' : $options[url_base];
-    $login = empty($options[login]) ? 'admin' : $options[login];
-    $password = empty($options[password]) ? 'admin' : $options[password];
-    $cache_duration = empty($options[cache_duration]) ? 3600 : $options[cache_duration];
-
-    if ( !empty( $title ) ) { echo $before_title . $title . $after_title; };
+    $url_base = empty( $options["url_base"] ) ? 'localhost' : $options["url_base"];
+    $login = empty($options["login"]) ? 'admin' : $options["login"];
+    $password = empty($options["password"]) ? 'admin' : $options["password"];
+    $cache_duration = empty($options["cache_duration"]) ? 3600 : $options["cache_duration"];
 
     // TODO : do not show anything if no password, send an error message on screen
 
@@ -51,14 +93,9 @@ function collectiveaccess_search_results($v, $url)
         $pages = floor($num_results / $num_per_page);
         //var_dump($page);die();
         $vignettes .= "<p>".$num_results." results</p>";
-        $body .= "<ul>";
         $i = 1;
         foreach($result_data as $result) {
             if (floor(($i-1)/$num_per_page) == $page) {
-                $body .= "<li><a href=\"".get_site_url()."/collections/object/detail/".$result["id"]."\" >".
-                    $result["display_label"].
-                    ($result["idno"]? " <small>(".$result["idno"].")</small>" : "").
-                    "</a></li>\n";
                 $vignettes .= "<figure class='gallery-item'> <div class='gallery-icon landscape'> ";
                 $vignettes .= "<a href=\"".get_site_url()."/collections/object/detail/".$result["id"]."\" > \n";
                 $vignettes .= "<div class='collectiveaccess-cropping-image'><img class=\"attachment-thumbnail\" data-table=\"ca_objects\" data-id=\"".$result["id"]."\"/> </span>\n";
@@ -68,53 +105,25 @@ function collectiveaccess_search_results($v, $url)
             }
             $i++;
         }
-        $body .= "</ul>";
+
+        // Inserting encapsulation div
         $vignettes ="<div id='gallery-1' class='gallery galleryid-555 gallery-columns-3 gallery-size-thumbnail'>".$vignettes."</div>\n";
+
+        // Page navigation
         $vignettes .= "<div class=\"page-links\"><span class=\"page-links-title\">Pages:</span>\n";
+        // Inserting hidden form for page navigation, keeping query
         $vignettes .= "<FORM name=\"page\" action=\"".get_site_url()."/collections/objects/search\" method=\"post\">\n".
             "<input type=\"hidden\" name=\"query\" value=\"".$query."\">".
             "<input type=\"hidden\" name=\"page\" id=\"page\" value=\"1\">".
             "</FORM>";
+        // Inserting page links
         for ($i = 1; $i <= $pages; $i++) {
             $vignettes .= "<a href='#' onclick=\"document.forms['page'].page.value = $i;document.forms['page'].submit();\"><span>".$i."</span></a>\n";
         }
         $vignettes .= "</div>";
-        $v->title = "Results for ".$query;
-        $vignettes.= "<style>figure.gallery-item p {display:none;}</style>";
-        $vignettes .=
-"<style>
-.collectiveaccess-cropping-image {
-    height:170px;
-    width:150px;
-    overflow:hidden;
-    background-color: black;
-    text-align: center;
-    vertical-align: middle;
-    display:table-cell;
-}
-</style>";
-        $vignettes.="
-<script type='text/javascript'>
-    jQuery(document).ready(function() {
-        jQuery('img.attachment-thumbnail').each(function() {
-            var getimage_table = jQuery(this).data(\"table\");
-            var getimage_id = jQuery(this).data(\"id\");
-            var jquery_this = jQuery(this);
-            jQuery.ajax({
-                type:'POST',
-                dataType: 'text',
-                url:'" . get_site_url() . "/wp-content/plugins/WP-CollectiveAccess/collectiveaccess_ajax_handler.php',
-                data: {
-                    action:'getimage',
-                    table:getimage_table,
-                    id:getimage_id
-                },
-                success:function(response){jquery_this.attr('src',JSON.parse(response))}
-            });
-        });
-    });
 
-</script>";
+        $title = "Results for ".$query;
+        $v->title = $title;
         $v->body = $vignettes; //   $body.
 
     } elseif (!$url_base) {
