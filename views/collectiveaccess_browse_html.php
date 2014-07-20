c92ab3dbecdfe2f7cfd6898d465211f238d7f866
GautierMichelin <gm@ideesculture.com>
2014-07-20 08:10:23 +0200

    Reordering print order

--- views/collectiveaccess_browse_html.php
+++ views/collectiveaccess_browse_html.php
@@ -2,9 +2,12 @@
 	$removecriterias = $this->getVar("removecriterias");
 	$thumbnails = $this->getVar("thumbnails");
 	$pagination = $this->getVar("pagination");
+	$facets_content = $this->getVar("facets_content");
+	$have_results = $this->getVar("have_results");
 ?>
 <?php print $removecriterias; ?>
-
-<?php print $thumbnails; ?>
-
+<?php print $facets_content; ?>
+<?php 
+	if($have_results) print $thumbnails;		
+?>
 <?php print $pagination; ?>
