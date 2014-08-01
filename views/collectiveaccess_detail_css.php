06154ffc09862efbc93e22b6ec1530d56c295df6
GautierMichelin <gm@ideesculture.com>
2014-08-01 18:13:47 +0200

    Adding some CSS for tilepic iframe

---
+++ views/collectiveaccess_detail_css.php
@@ -0,0 +1,35 @@
+#wpca-tilepic-iframe {
+    width:100%;
+    height:100%;
+}
+#wpca-tilepic-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #000000;
+    background-color: rgba(255,255,255,0.97);
+    z-index: 10000;
+}
+#wpca-tilepic-close {
+    margin-top:35px;
+    position:absolute;
+    right:0;
+    padding:10px;
+}
+#wpca-tilepic-close A {
+    font-weight:bold;
+    color:white;
+    padding:10px;
+    border-radius:6px;
+    background-color:#222222;
+}
+#wpca-tilepic-iframe {
+    
+}
+
+#caMediaOverlayContent {
+    width:100%;
+    height:100%;
+}
