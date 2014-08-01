d37fc09316fc6468897b4f02de5ec5904fc9945b
GautierMichelin <gm@ideesculture.com>
2014-08-01 18:14:39 +0200

    Calling tilepicviewer iframe through jquery

---
+++ views/collectiveaccess_detail_js.php
@@ -0,0 +1,18 @@
+<?php
+	$tilepicphp_url = plugins_url( 'tilepic.php', WP_CA_MAIN_FILE);
+    $tpc_image_url = $this->getVar('tilepic_image_url');
+    $tpc_height = $this->getVar('tilepic_height');
+    $tpc_width = $this->getVar('tilepic_width');
+    $tpc_remoteviewer_url = $this->getVar('tilepic_remoteviewer_url');
+    $url = $tilepicphp_url."?tpc=".urlencode($tpc_image_url)."&viewer=".urlencode($tpc_remoteviewer_url)."&height=".$tpc_height."&width=".$tpc_width;
+?>
+
+jQuery(document).ready(function(){
+    jQuery('#mediaview').on("click", function(e) {
+        var overlay = jQuery("<div id='wpca-tilepic-overlay'><div id='wpca-tilepic-close'><a href='#'>close</a></div><iframe id='wpca-tilepic-iframe' src='<?php print $url; ?>' /></div>");
+        overlay.appendTo(document.body);
+        jQuery('#wpca-tilepic-close').on("click", function(e) {
+            jQuery("#wpca-tilepic-overlay").remove();
+        });
+    });
+});
