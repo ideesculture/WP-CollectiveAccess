4cd85a56506f9e6654404e2c41f72b2bbb6d82a4
GautierMichelin <gm@ideesculture.com>
2014-08-03 12:15:56 +0200

    Merging some css inside common one

--- views/collectiveaccess_css.php
+++ views/collectiveaccess_css.php
@@ -1,3 +1,10 @@
+<?php
+    // asset path
+    $expand_icon_url = plugins_url( 'assets/images/icon-expand.png', WP_CA_MAIN_FILE);
+    $loading_icon_url = plugins_url( 'assets/images/icon-loading.png', WP_CA_MAIN_FILE);
+    $shrink_icon_url = plugins_url( 'assets/images/icon-shrink.png', WP_CA_MAIN_FILE);
+    $empty_icon_url = plugins_url( 'assets/images/icon-empty.png', WP_CA_MAIN_FILE);
+?>
 
 figure.gallery-item p {display:none;}
 
@@ -66,3 +73,40 @@ p.facetname:first-child {
     background-color: rgb(65, 166, 42);
     color:#fff;
 }
+
+.wpca-parent-container {
+    margin:4px 0 4px 20px;
+}
+
+.toggler {
+    background:#009112;
+    margin-right:4px;
+    padding:3px 3px 0 3px;
+    border-radius:3px;
+}
+
+.fold > .toggler-icon {
+    content:url('<?php print $expand_icon_url; ?>');
+}
+.unfold > .toggler-icon {
+    content:url('<?php print $shrink_icon_url; ?>');
+}
+.empty > .toggler-icon {
+    content:url('<?php print $empty_icon_url; ?>');
+}
+
+.loading > .toggler-icon {
+    content:url('<?php print $loading_icon_url; ?>');
+    -webkit-animation: spin 0.5s infinite linear;
+    -moz-animation: spin 0.5s infinite linear;
+    -ms-animation: spin 0.5s infinite linear;
+    animation: spin 0.5s infinite linear;
+}
+
+
+.wpca-parent-title {
+}
+
+@-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
+@-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
+@keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
