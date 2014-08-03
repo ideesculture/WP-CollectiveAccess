19349e1c83aa70096f2ed1312bbd910cc14a0086
GautierMichelin <gm@ideesculture.com>
2014-08-03 12:17:05 +0200

    Merging with hierarchy tree js

--- views/collectiveaccess_js.php
+++ views/collectiveaccess_js.php
@@ -11,8 +11,6 @@ jQuery(document).ready(function(){
         jQuery(this).toggleClass('collapsed');
         return true;
     });        
-});
-jQuery(document).ready(function() {
     jQuery('img.attachment-thumbnail').each(function() {
         var getimage_table = jQuery(this).data("table");
         var getimage_id = jQuery(this).data("id");
@@ -35,5 +33,35 @@ jQuery(document).ready(function() {
                 }
             }
         });
+    });
+    jQuery('#wpca_hierarchy').on('click','.toggler.fold',function() {
+        console.log(jQuery(this));
+        var container_table = jQuery(this).parent().data("table");
+        var container_id = jQuery(this).parent().data("id");
+        jQuery(this).removeClass('fold').addClass('loading spin');
+        jQuery.ajax({
+            type:'POST',
+            dataType: 'json',
+            url:'<?php print plugins_url( 'collectiveaccess_ajax_handler.php' ,  WP_CA_MAIN_FILE); ?>',
+            data: {
+                action:'getchildren',
+                table:container_table,
+                id:container_id
+            },
+            success:function(response){
+                if(response != '-1') {
+                    jQuery('#wpca-parent-container_'+container_table+'_'+container_id).append(response);
+                    jQuery('#wpca-parent-container_'+container_table+'_'+container_id+' > .toggler').removeClass('loading spin').addClass('unfold');
+                } else {
+                    jQuery('#wpca-parent-container_'+container_table+'_'+container_id+' > .toggler').removeClass('loading spin').addClass('empty');
+                }
+            }
+        });
+       
+    });
+    jQuery('#wpca_hierarchy').on('click','.toggler.unfold',function() {    
+        console.log(jQuery(this));
+        jQuery(this).parent().children('.wpca-parent-container').remove();
+        jQuery(this).removeClass('unfold').addClass('fold');
     });
 });
