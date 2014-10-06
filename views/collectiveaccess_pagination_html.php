4c617a8441f680adda44f19d8892f5c84a154a72
ilboga <michaelbogani@gmail.com>
2014-10-06 16:08:05 +0200

    add current page on pagination view

--- views/collectiveaccess_pagination_html.php
+++ views/collectiveaccess_pagination_html.php
@@ -1,14 +1,16 @@
 <?php
-    $pages = $this->getVar("pages");
-    $formname = $this->getVar("formname");
+    $pages 			= $this->getVar("pages");
+    $current_page 	= $this->getVar("current_page");
+    $formname 		= $this->getVar("formname");
 ?>
 </form>
 <div class="page-links"><span class="page-links-title"><?php _e("Pages:","collectiveaccess") ?></span>
 <?php
     // Loop through pages
     for ($i = 1; $i <= $pages; $i++) : ?>
-<a href='#' onclick="document.forms['<?php print $formname; ?>'].page.value = <?php print $i; ?>;document.forms['browse_facets'].submit();"><span><?php print $i; ?></span></a>
-<?php 
-    endfor; 
-?>
+    	<? $current = ""; if ($current_page == $i) $current = "class=\"current\""; ?> 
+		<a href='#' <?php echo $current; ?> onclick="document.forms['<?php print $formname; ?>'].page.value = <?php print $i; ?>;document.forms['browse_facets'].submit();">
+			<span><?php print $i; ?></span>
+		</a>
+<?php  endfor; ?>
 </div>
